ce95bbb9b3a64311ccb834b58a3e0fef7e364800
ksostuft <ksostuft@gmail.com>
2023-10-06 17:44:59 +0900

    Initial commit

---
+++ src/test/java/com/ohgiraffers/practice20231006/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.practice20231006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
